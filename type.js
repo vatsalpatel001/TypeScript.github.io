41be148e4057028dea8409c93700f2d51abe2946
vatsal <vatsalpatel001@gmail.com>
2020-12-17 17:16:17 +0530

    learnt interface

--- type.js
+++ type.js
@@ -8,3 +8,15 @@ console.log(y.charAt(1));
 console.log(y.charAt(1));
 y = 5;
 console.log(y.toFixed(1));
+var xc = function (y) {
+    console.log(y);
+};
+var xc1 = function (y) {
+    console.log(y);
+};
+var xc2 = function (y) { return console.log(y); };
+xc2("arrroeee");
+var PrintVal1 = function (objq) {
+    console.log(objq);
+};
+PrintVal1({ firstnmae: "Vatsal", lastname: "Patel" });
--- type.ts
+++ type.ts
@@ -5,7 +5,31 @@ x=5;
 // x="hj"
 let y;
 y="67dfgdfg";
-console.log((<string>y).charAt(1);)
-console.log((y as string).charAt(1);)
+console.log((<string>y).charAt(1))
+console.log((y as string).charAt(1))
 y=5;
-console.log((<number>y).toFixed(1);)
+console.log((<number>y).toFixed(1))
+
+var xc = function(y){
+    console.log(y)
+}
+
+var xc1 = (y) => {
+    console.log(y)
+}
+
+var xc2 = (y:string) =>  console.log(y);
+
+xc2("arrroeee");
+
+
+interface objq{
+    firstnmae:string,
+    lastname:string
+}
+
+var PrintVal1 = (objq:objq) => {
+    console.log(objq)
+}
+
+PrintVal1({firstnmae:"Vatsal",lastname:"Patel"})
