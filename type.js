cc79b3dab8e1775d4ef00ddb2a042b24ebb1b416
vatsal <vatsalpatel001@gmail.com>
2020-12-17 12:33:27 +0530

    learnt Type Assertion in Typescript

--- type.js
+++ type.js
@@ -1,1 +1,10 @@
 console.log("hello");
+var x;
+x = 5;
+// x="hj"
+var y;
+y = "67dfgdfg";
+console.log(y.charAt(1));
+console.log(y.charAt(1));
+y = 5;
+console.log(y.toFixed(1));
--- type.ts
+++ type.ts
@@ -1,1 +1,11 @@
-console.log("hello")
+console.log("hello");
+
+var x:number;
+x=5;
+// x="hj"
+let y;
+y="67dfgdfg";
+console.log((<string>y).charAt(1);)
+console.log((y as string).charAt(1);)
+y=5;
+console.log((<number>y).toFixed(1);)
